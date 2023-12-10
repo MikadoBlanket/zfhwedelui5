c04d05d7e19683c086259a78dad614663f1c7b6a
CPUCloudCode <lenni.meissner@gmail.com>
2023-12-10 16:30:56 +0100

    Internes JSON-Model (Aufgabe 2)

--- zfhwedel-ui5/webapp/controller/Main.controller.js
+++ zfhwedel-ui5/webapp/controller/Main.controller.js
@@ -1,16 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, JSONModel) {
         "use strict";
 
         return Controller.extend("zfhwedel.zfhwedelui5.controller.Main", {
             onInit: function () {
-
+                 // set data model on view
+                 const oData = {
+                    recipient: {
+                        name: "World"
+                    }
+                }
+                const oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
             },
 
             onShowHello: function () {
