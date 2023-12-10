dae54ba5c60ff47e7428b8645b84dd5f3faa0c8f
CPUCloudCode <lenni.meissner@gmail.com>
2023-12-10 16:27:39 +0100

    Hello World Button (Aufgabe 1)

--- zfhwedel-ui5/webapp/controller/Main.controller.js
+++ zfhwedel-ui5/webapp/controller/Main.controller.js
@@ -1,15 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("zfhwedel.zfhwedelui5.controller.Main", {
             onInit: function () {
 
+            },
+
+            onShowHello: function () {
+                MessageToast.show("Hello World")
             }
         });
     });
